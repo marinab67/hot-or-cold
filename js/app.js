78f647e79f95d48b77b5e13bf932eb2720783144
marinab67 <marinab67@gmail.com>
2015-04-26 23:32:38 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -12,6 +12,30 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	function newGame () { 
+  		x = Math.floor((Math.random()*100) + 1);
+  		console.log(x); 
+  	}
+
+  	//Starts a new game on load//
+  	newGame();
+  
+
+  	function hotOrCold (number) {  
+  		if (isNaN($("#userGuess").val()) || ($("#userGuess").val())%1!=0) {alert("enter a whole number!!! thx."); }
+  		else { 
+	  		number = parseInt($("#userGuess").val());
+	  		var difference = Math.abs(number - x);
+	  		if (difference > 49) {alert("your guess is ice cold lol~"); }
+	  		else if (29 < difference < 50) {alert("cold"); }
+	  		else if (19 < difference < 30) {alert("warm"); }
+	  		else if (9 < difference < 20) {alert("hot") ;}
+	  		else {alert("very hot"); }
+  		}
+  	}
+
+  	$("#guessButton").on("click", hotOrCold);
+
 });
 
 
